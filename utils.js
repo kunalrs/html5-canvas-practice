e968f0b27b1b2b22c0db4507a0e9dc05a98261e8
Kunal <sagar.kunal@yahoo.com>
2020-05-10 15:27:58 +0530

    Sine Wave animation

--- utils.js
+++ utils.js
@@ -1,5 +1,29 @@
 let utils = {};
 
+/**
+ * Returns a color in the format: '#RRGGBB', or as a hex number if specified.
+ * @param {number|string} color
+ * @param {boolean=}      toNumber=false  Return color as a hex number.
+ * @return {string|number}
+ */
+utils.parseColor = function (color, toNumber) {
+    if (toNumber === true) {
+        if (typeof color === 'number') {
+            return (color | 0); //chop off decimal
+        }
+        if (typeof color === 'string' && color[0] === '#') {
+            color = color.slice(1);
+        }
+        return window.parseInt(color, 16);
+    } else {
+        if (typeof color === 'number') {
+            color = '#' + ('00000' + (color | 0).toString(16)).substr(-6); //pad
+        }
+        return color;
+    }
+};
+
+
 utils.captureMouse = function(element) {
     let mouse = {x: 0, y: 0};
     element.addEventListener("mousemove", function(event){
